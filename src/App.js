86ac17d934167dbc1f6835a104b886d3e644ff05
felixghislaindev <felixghislain@yahoo.com>
2021-04-28 14:51:11 -0700

    Generate dummy data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import './App.css';
+import React from 'react';
+import Card from './components/card/Card-component'
 
 function App() {
+  const [flickrPhotos,setFlickrPhotos] = React.useState([
+    {
+      "title":"in the park",
+      "author":"Mike",
+      "description":"Nice walk at the park",
+      "tags":["run","walk","morning"]
+  },
+  {
+    "title":"in the forest",
+    "author":"Marta",
+    "description":"Nice walk at the rainy forest",
+    "tags":["run","walk","morning coffee"]
+},
+{
+  "title":"in the sun ",
+  "author":"jsosh",
+  "description":"Nice walk on the beach",
+  "tags":["run","walk","barcelona","sun"]
+},
+{
+  "title":"cold mornings",
+  "author":"Fabian",
+  "description":"Snowy morning",
+  "tags":["run","walk","cold","coffee"]
+}
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flickr-photo-card">
+    {
+      flickrPhotos.map((flickrPhoto, index) =>(
+      <Card key={index} index={index} card={flickrPhoto}/>
+      ))
+      }
+    </div>
+      
     </div>
   );
 }
---
+++ src/components/card/Card-component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const Card = ({card}) => {
+    return ( 
+        <div className="card">
+            {card.title}
+        </div>
+     );
+}
+ 
+export default Card;
